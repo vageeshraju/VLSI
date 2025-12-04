5f44aa1bfc40a02c063d05b5ea6bfa96ca1ca671
vageeshraju <vageeshraju05@gmail.com>
2025-12-04 11:34:30 +0530

    Create 10.mux2X1_tb.v

---
+++ combinationl_circuit/10.mux2X1_tb.v
@@ -0,0 +1,19 @@
+module mux2X1_tb();
+reg sel,a,b;
+wire y;
+mux2X1 dut (
+.sel(sel),
+.a(a),
+.b(b),
+.y(y));
+initial begin
+$dumpfile("mux2X1.vcd");
+$dumpvars(0,mux2X1_tb);
+$monitor ("sel=%b,a=%b,b=%b,y=%b",sel,a,b,y);
+#5sel=0;a=0;b=1;
+#5sel=1;a=0;b=1;
+#5sel=0;a=1;b=0;
+#5sel=1;a=1;b=0;
+#20 $finish;
+end
+endmodule           
