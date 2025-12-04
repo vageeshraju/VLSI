4cb34c6cbc7fa5f3367a7bcae36303f1283761fd
vageeshraju <vageeshraju05@gmail.com>
2025-12-04 10:19:50 +0530

    Update fullader_tb.v

--- combinationl_circuit/fullader_tb.v
+++ combinationl_circuit/fullader_tb.v
@@ -1,16 +1,16 @@
 module full_adder_tb();
 reg a,b,cin;
-wire sum,carry;
+wire sum,c_out;
 full_adder dut (
 .a(a),
 .b(b),
 .cin(cin),
 .sum(sum),
-.carry(carry));
+.c_out(c_out));
 initial begin
 $dumpfile ("fulladder.vcd");
 $dumpvars (0,full_adder_tb);
-  $monitor ("a=%b,b=%b,cin=%b,sum=%b,carry=%b",a,b,cin,sum,carry);
+  $monitor ("a=%b,b=%b,cin=%b,sum=%b,c_out=%b",a,b,cin,sum,c_out);
 #5a=0;b=0;cin=0;
 #5a=0;b=1;cin=0;
 #5a=1;b=0;cin=0;
